666d2dc0843ff7608e164f1d345fb6e011ea19cf
NijiRaku Makulay <pgmrigor@gmail.com>
2025-11-29 10:10:23 +0800

    Completed the lobby

--- Assets/Scripts/Lobby/PhotonLoby.cs
+++ Assets/Scripts/Lobby/PhotonLoby.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.SceneManagement;
 
 public class PhotonLoby : MonoBehaviourPunCallbacks
 {
@@ -22,10 +23,19 @@ public class PhotonLoby : MonoBehaviourPunCallbacks
     public TMP_InputField inputID;
     public TMP_InputField joinID;
 
+    [Header("UI Message References")]
+    public TMP_Text sysmsg;
+    const string CreateMSG = "Creating a room...";
+    const string JoinMSG = "Joining a room...";
+    const string ErrorMSGA = "Why not you create that room while it doesn't exist yet?";
+    const string ErrorMSGB = "It's too quiet. Please try creating a room.";
+    const string ErrorMSGC = "Isn't that room already created? Please try joining it.";
+
     private void Awake()
     {
         lobby = this;
         PhotonNetwork.AutomaticallySyncScene = true;
+        sysmsg.text = "Welcome to FragZone!";
     }
 
     private void Start()
@@ -38,9 +48,15 @@ public class PhotonLoby : MonoBehaviourPunCallbacks
     {
         Debug.Log("Player has connected to the Photon Master server");
         //PhotonNetwork.AutomaticallySyncScene = true;
+        LobbyMenuOnlineState();
+        offlineButton.SetActive(false); //this will be disabled if we are connected to the server
+    }
+
+    private void LobbyMenuOnlineState()
+    {
         createButton.SetActive(true); //this enables the start button allowing players to create a network  lobby
         joinButton.SetActive(true);//this enables the join button allowing players to join a network  lobby
-        offlineButton.SetActive(false); //this will be disabled if we are connected to the server
+        cancelButton.SetActive(false);
     }
 
     //button script for connecting to a network game
@@ -48,21 +64,24 @@ public class PhotonLoby : MonoBehaviourPunCallbacks
     {
         Debug.Log("Play Button was clicked");
         Debug.Log("Creating a room...");
+        sysmsg.text = CreateMSG;
         CreateRoom();
+        LobbyMenuProcessState();
+    }
 
+    private void LobbyMenuProcessState()
+    {
         createButton.SetActive(false);
         joinButton.SetActive(false);
         cancelButton.SetActive(true);
     }
+
     //button script for joining a network game
     public void BtnJoinRoom()
     {
         Debug.Log("Joining a room...");
         JoinRoomById();
-
-        createButton.SetActive(false);
-        joinButton.SetActive(false);
-        cancelButton.SetActive(true);
+        LobbyMenuProcessState();
     }
 
 
@@ -71,16 +90,22 @@ public class PhotonLoby : MonoBehaviourPunCallbacks
     public void OnCancelButtonClicked()
     {
         Debug.Log("Cancel Button Clicked");
-        cancelButton.SetActive(false);
         PhotonNetwork.LeaveRoom();
-        createButton.SetActive(true);
-        joinButton.SetActive(true);
+        LobbyMenuOnlineState();
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         Debug.Log("Tried to join a random game but failed. There must be no open room");
-        CreateRoom();
+        sysmsg.text = ErrorMSGB + "\n(" + message + ")";
+        LobbyMenuOnlineState();
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("How about you create that non-existing room?");
+        sysmsg.text = ErrorMSGA + "\n(" + message + ")";
+        LobbyMenuOnlineState();
     }
 
     void CreateRoom()
@@ -95,9 +120,9 @@ public class PhotonLoby : MonoBehaviourPunCallbacks
         {
             IsVisible = true,
             IsOpen = true,
-            MaxPlayers = 2
+            MaxPlayers = 5
         };
-
+        sysmsg.text = CreateMSG+"Room "+roomName+" (Max "+roomOps.MaxPlayers+" Players)";
         PhotonNetwork.CreateRoom(roomName, roomOps);
         Debug.Log($"Trying to create room: {roomName}");
     }
@@ -109,10 +134,12 @@ public class PhotonLoby : MonoBehaviourPunCallbacks
         if (string.IsNullOrEmpty(roomToJoin)) //joins a  random room if no input data detected
         {
             Debug.Log("No room name entered → joining a random open room...");
+            sysmsg.text = JoinMSG + " (Quick Join)";
             PhotonNetwork.JoinRandomRoom(); // will trigger OnJoinRandomFailed if none exists
         }
         else //this allows us to connect to the specified room
         {
+            sysmsg.text = JoinMSG + "Room "+roomToJoin;
             Debug.Log($"Trying to join specific room: {roomToJoin}");
             PhotonNetwork.JoinRoom(roomToJoin);
         }
@@ -122,12 +149,13 @@ public class PhotonLoby : MonoBehaviourPunCallbacks
     {
         Debug.Log("We are now in a room");
         PhotonNetwork.LoadLevel(sceneToLoad);
+        SceneManager.LoadScene(sceneToLoad);
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         Debug.Log("Tried to create a new room but failed, there must be a room with the same name");
-        CreateRoom();
+        sysmsg.text = ErrorMSGC + "\n(" + message + ")";
     }
 
 
